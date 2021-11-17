966ee7530c08ed7c9358ef149513d3c9a007016a
jddadev <joethebartenderpdx@gmail.com>
2021-11-16 18:13:28 -0800

    Slight design chage to Header

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import { LogoutIcon } from '@heroicons/react/solid';
 function Header() {
   const router = useRouter();
   return (
-    <header className="sticky bg-yellow-200 z-50 grid grid-cols-3 items-center shadow-md text-red-400 font-architect">
+    <header className="sticky bg-yellow-200 z-50 grid grid-cols-3 items-center justify-between shadow-md text-red-400 font-architect">
       <div
         onClick={() => router.push('/ ')}
         className="relative drop-shadow-lg  flex items-center cursor-pointer text-6xl font-semibold  p-5 hover:scale-105 transition transform duration-150 ease-out"
@@ -14,13 +14,15 @@ function Header() {
         SQGGLE
       </div>
 
-      <div className="text-4xl flex justify-end py-4 items-center">
-        An Online Drawing Prompter
+      <div className="text-4xl text-center py-4 items-center">
+        <p className=""> An Online Drawing Prompter</p>
       </div>
 
-      <div className="flex flex-row items-end justify-evenly border-2 border-red-200 rounded-full w-1/2 pr-5 mr-5">
-        <LoginIcon className="h-8 pr-7 cursor-pointer" />
-        <LogoutIcon className="h-8 pr-7 cursor-pointer" />
+      <div className="flex items-center justify-end px-4">
+        <div className="flex items-center space-x-2 border-2 border-red-200 rounded-full p-2">
+          <LoginIcon className="h-8 cursor-pointer" />
+          <LogoutIcon className="h-8 cursor-pointer" />
+        </div>
       </div>
     </header>
   );
