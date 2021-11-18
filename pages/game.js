a5472e4c1d8a63961dceb2cbac62fca4d2e3553e
jddadev <joethebartenderpdx@gmail.com>
2021-11-17 18:38:52 -0800

    moved counter logic to game, passed it to timer component

--- pages/game.js
+++ pages/game.js
@@ -4,14 +4,28 @@ import { createScene } from '../utils/prompt';
 
 function Game() {
   const [scene, setScene] = useState('');
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     const scene = createScene();
     setScene(scene);
   }, []);
+
+  useEffect(() => {
+    let interval = null;
+    if (seconds < 5) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else {
+      return;
+    }
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
     <div>
-      <Timer />
+      <Timer seconds={seconds} />
       <h1>{scene}</h1>
     </div>
   );
